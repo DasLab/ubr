4395b8e6ec846a8411c4cf8d452f61d40d4fcb38
Rhiju Das <rhiju@stanford.edu>
2023-06-09 09:57:59 -0700

    by default output all sequences to csv if good_idx is empty

--- matlab/output/output_examples_csv.m
+++ matlab/output/output_examples_csv.m
@@ -1,6 +1,8 @@
 function output_examples_csv(datafile,good_idx,ids,sequences,mfe_tags,mfe_structures,mfe_tags_to_output);
-%
+% output_examples_csv(datafile,good_idx,ids,sequences,mfe_tags,mfe_structures,mfe_tags_to_output);
 
+if isempty(good_idx); good_idx = [1:length(sequences)]; end;
+if size(good_idx,2)>size(good_idx,1); good_idx = good_idx'; end;
 fid = fopen( datafile,'w');
 fprintf(fid,'eterna_id,sequence');
 for k = 1:length(mfe_tags_to_output)
