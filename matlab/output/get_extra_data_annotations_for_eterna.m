b28fb60f40702bdfaa0871c5b09d44c7eb79e3f1
Rhiju Das <rhiju@stanford.edu>
2023-08-22 10:41:58 -0700

    output all profiles to RDAT by default; bad S/N flagged with annotation warning:badQuality

--- matlab/output/get_extra_data_annotations_for_eterna.m
+++ matlab/output/get_extra_data_annotations_for_eterna.m
@@ -1,5 +1,12 @@
-function extra_data_annotations = get_extra_data_annotations_for_eterna( ids, titles, authors, extra_info_structs )
+function extra_data_annotations = get_extra_data_annotations_for_eterna( ids, titles, authors, extra_info_structs, headers )
 % extra_data_annotations = get_extra_data_annotations_for_eterna( ids, titles, authors, extra_info_structs )
+% extra_data_annotations = get_extra_data_annotations_for_eterna( ids, titles, authors, extra_info_structs, headers )
+% extra_data_annotations = get_extra_data_annotations_for_eterna( [], [], [], [], headers )
+%
+% Create cell of cells og tag strings  with tags like:
+%     Eterna:ids:, Eterna:titles:, Eterna:authors:;
+%     additional tags in extra_info_strings and, 
+%     if ids is not defined, name: tags based on headers
 %
 % Inputs
 %  ids = (list of Ndesigns numbers) Eterna ids 
@@ -7,27 +14,33 @@ function extra_data_annotations = get_extra_data_annotations_for_eterna( ids, ti
 %  authors = (cell of Ndesigns strings)  Eterna authors 
 %  extra_info_structs = Optional (cell of structs) all fields will be reformatted as
 %                   annotation strings
+%  headers = Optional (cell of strings) full FASTA headers. "Backup" 
 %
 % Output
-%  extra_data_annotations = (cell of Ndesigns cells) cells of tag strings
-%
+%  extra_data_annotations = (cell of Ndesigns cells) cells of cells of tag strings
 %
 % (C) R. Das, 2023, HHMI & Stanford University..
 %
 if ~exist( 'extra_info_structs','var'); extra_info_structs = []; end;
+if ~exist( 'headers','var'); headers = {}; end;
 if nargin == 0; help( mfilename ); return; end;
 
 Ndesigns = length(ids);
 assert( length(ids)  == Ndesigns );
 assert( length(titles)  == Ndesigns );
 assert( length(authors) == Ndesigns );
+if length(ids) == 0;  Ndesigns = length(headers); end
 
 extra_data_annotations = {};
 for i = 1:Ndesigns
     extra_data_annotation = {};
-    extra_data_annotation = [extra_data_annotation, {sprintf('Eterna:id:%d',ids(i))} ];
-    extra_data_annotation = [extra_data_annotation, {sprintf('Eterna:design_name:%s',titles{i})} ];
-    extra_data_annotation = [extra_data_annotation, {sprintf('Eterna:author:%s',authors{i})} ];
+    if ~isempty(ids) & length(ids) >= i && ~isnan(ids(i))
+        extra_data_annotation = [extra_data_annotation, {sprintf('Eterna:id:%d',ids(i))} ];
+        extra_data_annotation = [extra_data_annotation, {sprintf('Eterna:design_name:%s',titles{i})} ];
+        extra_data_annotation = [extra_data_annotation, {sprintf('Eterna:author:%s',authors{i})} ];
+    elseif  ~isempty(headers) & length(headers) >= i        
+        extra_data_annotation = [extra_data_annotation, {sprintf('name:%s',strrep(headers{i},sprintf('\t'),';'))} ];
+    end
 
     if i <= length(extra_info_structs);    
         extra_data_annotation = [extra_data_annotation, convert_struct_to_annotations( extra_info_structs{i}, 'Eterna' ) ];
--- matlab/output/output_rdats_from_ubr_analysis.m
+++ matlab/output/output_rdats_from_ubr_analysis.m
@@ -1,9 +1,12 @@
-function output_rdats_from_ubr_analysis( d, out_prefix, name, output_idx, comments, annotations, condition_comments, condition_annotations, chunk_size, signal_to_noise_cutoff, reads_cutoff);
-% output_rdats_from_ubr_analysis( d, out_prefix, name, comments, annotations, condition_comments, condition_annotations );
+function output_rdats_from_ubr_analysis( d, out_prefix, name, output_idx, comments, annotations, condition_comments, condition_annotations, chunk_size, signal_to_noise_cutoff, reads_cutoff, output_bad_profiles);
+% output_rdats_from_ubr_analysis( d, out_prefix, name, comments, annotations );
+% output_rdats_from_ubr_analysis( d, out_prefix, name, output_idx, comments, annotations, condition_comments, condition_annotations, chunk_size, signal_to_noise_cutoff, reads_cutoff, output_bad_profiles);
 %
 % Output background-subtracted data from UBR analysis to RDAT format,
 %  for profiles that surpass coverage, reads, and signal/noise criteria.
 % One RDAT per condition.
+% Note that all profiles will be outputted but 'bad' profiles will be
+%  marked with the tag "warning:badQuality"
 %
 % Inputs
 %  d      = MATLAB struct() that is output from quick_look_ubr(). 
@@ -24,6 +27,9 @@ function output_rdats_from_ubr_analysis( d, out_prefix, name, output_idx, commen
 %  chunk_size = max number of sequences per RDAT file (default 10000).
 %  signal_to_noise_cutoff = Minimum signal-to-noise (default 1.0).
 %  reads_cutoff = Minimum reads (default 100).
+%  output_bad_profiles = Output all profiles, including ones that do not
+%      pass signal_to_noise and reads cutoffs (which will be marked by
+%      'warning:badQuality' in DATA_ANNOTATION). Default 1.
 %
 % (C) R. Das, Stanford & HHMI, 2023.
 
@@ -39,12 +45,14 @@ if ~exist( 'condition_annotations', 'var') | length(condition_annotations)==0
     condition_annotations = [];
     for i = 1:Nconditions; condition_annotations{i} = {}; end;
 end
-if ~exist( 'chunk_size', 'var') chunk_size = 10000; end;
+if ~exist( 'chunk_size', 'var') | chunk_size == 0; chunk_size = 10000; end;
 if ~exist( 'signal_to_noise_cutoff', 'var'); signal_to_noise_cutoff = 1; end
 if ~exist( 'reads_cutoff', 'var'); reads_cutoff = 100; end
 if length(condition_annotations) ~= Nconditions
     fprintf('Length of condition_annotations %d needs to equal the number of conditions %d.\n',length(condition_annotations),Nconditions)
 end
+if ~exist('output_bad_profiles','var'); output_bad_profiles = 1; end;
+if ~isfield(d,'ids'); d.ids = []; d.titles = []; d.authors = []; end;
 
 for i = 1:Nconditions
     good_idx = find( d.signal_to_noise(:,i) >= signal_to_noise_cutoff & d.reads(:,i) >= reads_cutoff);
@@ -53,21 +61,25 @@ for i = 1:Nconditions
     annotations_out = [annotations,condition_annotations{i}];
     comments_out = [comments,condition_comments{i}];
 
-    % TODO: This extra_data_annotations needs to be updated for cases where we
-    %  don't have Eterna integer ids. Could use id_strings instead, or
-    %  maybe full headers? Could be tricky with tabs.
-    extra_data_annotations = get_extra_data_annotations_for_eterna( d.ids, d.titles, d.authors );
+    extra_data_annotations = get_extra_data_annotations_for_eterna( d.ids, d.titles, d.authors, [], d.headers);
+
+    bad_idx = setdiff([1:Ndesigns],good_idx);
+    for n = bad_idx; extra_data_annotations{n} = [extra_data_annotations{n},{'warning:badQuality'}]; end
+
+    save_idx = [1:Ndesigns]';
+    if ~output_bad_profiles; save_idx = good_idx; end
+
     filename = sprintf('%s_%s.rdat',out_prefix,d.conditions{i});
 
     num_chunks = floor((length(output_idx)-1)/chunk_size)+1;
     for q = 1:num_chunks        
         chunk_idx = output_idx( ((q-1)*chunk_size+1):min(q*chunk_size,length(output_idx)) );
-        good_chunk_idx = intersect(chunk_idx, good_idx);
+        save_chunk_idx = intersect(chunk_idx, save_idx);
 
         filename_chunk = filename;
         if (num_chunks > 1) filename_chunk = strrep(filename,'.rdat',sprintf('_%05d.rdat',q) ); end;
 
-        rdat = output_rdat_from_ubr_analysis( filename_chunk, name, good_chunk_idx, ...
+        rdat = output_rdat_from_ubr_analysis( filename_chunk, name, save_chunk_idx, ...
             d.r_norm(:,:,i), d.r_norm_err(:,:,i), ...
             d.sequences, d.structures, d.BLANK_OUT5, d.BLANK_OUT3, ...
             comments_out, annotations_out, extra_data_annotations, d.reads );
