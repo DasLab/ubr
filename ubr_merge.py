bc259c7c543937e31348214f10a6b44222e50dd4
Rhiju Das <rhiju@stanford.edu>
2023-08-28 15:41:34 -0700

    allow ubr_merge.py to read in from multiple folders rather than subdirectories of single folder; for merging lanes.

--- ubr_merge.py
+++ ubr_merge.py
@@ -13,32 +13,40 @@ parser = argparse.ArgumentParser(
                     description = 'Merge outputs from split ubr_run directories',
                     epilog = 'Merge .muts.txt and .coverage.txt output files from split ubr_run job directories.')
 
-args = parser.add_argument( 'split_dir',default='UBR',help='name of directory with 001/,002/ job directories (default UBR)' )
+args = parser.add_argument( 'split_dir',default='UBR/',help='name of directory with 001/,002/ job subdirectories; if multiple directories, just look directly inside directories.',nargs='+' )
 args = parser.add_argument( '--merge_files' ,nargs='+',help='Filenames to find and merge')
 args = parser.add_argument( '-s','--setup_slurm', action='store_true',help='Instead of running, create sbatch script' )
 args = parser.add_argument('-j','--jobs_per_slurm_node', default=16,type=int )
 args = parser.parse_args()
 
+split_dirs = args.split_dir
+for (i,split_dir) in enumerate(split_dirs):
+    if split_dir[:-1] != '/': split_dirs[i]=split_dir+'/'
+if len(split_dirs) == 1: split_dirs[0] = split_dirs[0]+'*/'
+print(split_dirs)
+
 merge_files = args.merge_files
 
 if merge_files == None:
-    unique_files = []
-    for tag in ['coverage','muts']:
-        # Find all the relevant files
-        globfiles = sorted(glob.glob('%s/*/*.%s.txt' % (args.split_dir,tag)  ))
-        globfiles += sorted(glob.glob('%s/*/*.%s.txt.gz' % (args.split_dir,tag)  ))
-        # Get unique names
-        unique_files = unique_files + sorted(list(set(map( os.path.basename, globfiles ))))
+    for split_dir in split_dirs:
+        unique_files = []
+        subdir = ''
+        for tag in ['coverage','muts']:
+            # Find all the relevant files
+            globfiles = sorted(glob.glob('%s/*.%s.txt' % (split_dir,tag)  ))
+            globfiles += sorted(glob.glob('%s/*.%s.txt.gz' % (split_dir,tag)  ))
+            # Get unique names
+            unique_files = unique_files + sorted(list(set(map( os.path.basename, globfiles ))))
 
-    mut_types = ['AC','AG','AT','CA','CG','CT','GA','GC','GT','TA','TC','TG','ins','del']
-    for tag in mut_types:
-        # Find all the relevant files
-        globfiles = sorted(glob.glob('%s/*/raw_counts/*.%s.txt' % (args.split_dir,tag)  ))
-        globfiles += sorted(glob.glob('%s/*/raw_counts/*.%s.txt.gz' % (args.split_dir,tag)  ))
-        # Get unique names
-        unique_files = unique_files + sorted(list(set(map( os.path.basename, globfiles ))))
+        mut_types = ['AC','AG','AT','CA','CG','CT','GA','GC','GT','TA','TC','TG','ins','del']
+        for tag in mut_types:
+            # Find all the relevant files
+            globfiles = sorted(glob.glob('%s/raw_counts/*.%s.txt' % (split_dir,tag)  ))
+            globfiles += sorted(glob.glob('%s/raw_counts/*.%s.txt.gz' % (split_dir,tag)  ))
+            # Get unique names
+            unique_files = unique_files + sorted(list(set(map( os.path.basename, globfiles ))))
 
-    merge_files = unique_files
+        merge_files = unique_files
 
 print('\nWill merge:')
 for merge_file in merge_files:
@@ -51,7 +59,7 @@ if args.setup_slurm:
         sbatch_preface = '#!/bin/bash\n#SBATCH --job-name=ubr_merge\n#SBATCH --output=ubr_merge.o%%j\n#SBATCH --error=ubr_merge.e%%j\n#SBATCH --partition=biochem,owners\n#SBATCH --time=8:00:00\n#SBATCH -n %d\n#SBATCH -N 1\n\n' % len(merge_files)
         fid_slurm.write( sbatch_preface )
         for merge_file in merge_files:
-            command = 'ubr_merge.py %s --merge_files %s &' % ( args.split_dir, merge_file )
+            command = 'ubr_merge.py %s --merge_files %s &' % ( ' '.join(args.split_dir), merge_file )
             fid_slurm.write( command +'\n' )
         fid_slurm.write('\nwait\n')
         fid_slurm.close()
@@ -67,7 +75,7 @@ if args.setup_slurm:
         fid_sbatch_commands = open( 'sbatch_merge_commands.sh', 'w')
 
         for (i,merge_file) in enumerate(merge_files):
-            command = 'ubr_merge.py %s --merge_files %s &' % ( args.split_dir, merge_file )
+            command = 'ubr_merge.py %s --merge_files %s &' % ( ' '.join(args.split_dir), merge_file )
             fid_slurm.write( command +'\n' )
             if ( (i+1) % args.jobs_per_slurm_node == 0 ) or i == len(merge_files)-1:
                 fid_sbatch_commands.write('sbatch %s\n' % fid_slurm.name )
@@ -88,12 +96,18 @@ time_output = 0
 for filename in merge_files:
     time_startfile = time.time()
 
-    infiles = sorted(glob.glob('%s/*/%s' % (args.split_dir,filename) ))
-    outdir = ''
+    infiles = []
+    for split_dir in split_dirs:
+        infiles += sorted(glob.glob('%s/%s' % (split_dir,filename) ))
+        infiles += sorted(glob.glob('%s/%s.gz' % (split_dir,filename) ))
+        outdir = ''
+
     if len(infiles) == 0:
-        infiles = sorted(glob.glob('%s/*/raw_counts/%s' % (args.split_dir,filename) ))
-        outdir = 'raw_counts/'
-        os.makedirs( outdir, exist_ok = True )
+        for split_dir in split_dirs:
+            infiles += sorted(glob.glob('%s/raw_counts/%s' % (split_dir,filename) ))
+            infiles += sorted(glob.glob('%s/raw_counts/%s.gz' % (split_dir,filename) ))
+            outdir = 'raw_counts/'
+            os.makedirs( outdir, exist_ok = True )
 
     counts = []
     df = None
