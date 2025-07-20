37c326fbf81a1119545997f5dc8cd42932dff85d
Rhiju Das <rhiju@stanford.edu>
2025-07-20 16:26:12 -0700

    update combine_ubr_data and output_ubr_hdf5 to handle 2D data from m2_util

--- matlab/data/combine_ubr_data.m
+++ matlab/data/combine_ubr_data.m
@@ -3,6 +3,7 @@ function d_out = combine_ubr_data( d1, d2, RENORM )
 % d_out = combine_ubr_data( d1, d2 )
 %
 % Combines two data sets for the same sequences but in different conditions
+%  Also works on data sets with 2d r, r_norm data.
 %
 %  See also:
 %     MERGE_UBR_DATA to actually take averages.
@@ -28,44 +29,54 @@ if (nargin == 2 & isstruct(d2) ), all_d = {d1,d2}; end
 assert( iscell(all_d) );
 assert( length(all_d)>= 1);
 
-
 d_out = all_d{1};
 
 for q = 2:length( all_d )
     num_cidx = length(d_out.conditions);
-    num_tags = length(d_out.tags);
     assert( length(all_d{q}.sequences) == length(d_out.sequences) );
     assert( all(strcmp(all_d{q}.sequences,d_out.sequences)) );
-    d_out.r_norm = cat(3, d_out.r_norm, all_d{q}.r_norm);
-    d_out.r_norm_err = cat(3, d_out.r_norm_err, all_d{q}.r_norm_err);
+    NDIMS = length(size(d_out.r_norm));
+    d_out.r_norm = cat(NDIMS, d_out.r_norm, all_d{q}.r_norm);
+    d_out.r_norm_err = cat(NDIMS, d_out.r_norm_err, all_d{q}.r_norm_err);
     d_out.conditions =[d_out.conditions, all_d{q}.conditions];
     d_out.reads = cat(2, d_out.reads, all_d{q}.reads);
     d_out.signal_to_noise = cat(2, d_out.signal_to_noise, all_d{q}.signal_to_noise);
-    for n = 1:length( all_d{q}.shape_nomod_idx )
-        shape_nomod_idx = all_d{q}.shape_nomod_idx{n};
-        for k = 1:length(shape_nomod_idx); shape_nomod_idx(k) = shape_nomod_idx(k) + num_tags; end;
-        d_out.shape_nomod_idx{ num_cidx + n } = shape_nomod_idx;
-    end
+
+    if isfield(d_out,'coverage'); 
+        d_out.coverage = cat(2, d_out.coverage, all_d{q}.coverage);
+        d_out.total_coverage = d_out.total_coverage + all_d{q}.total_coverage;
+    end;
+
+    if ~isfield(d_out,'tags') continue; end
+    num_tags = length(d_out.tags);
+    num_shape_nomod_idx = length(d_out.shape_nomod_idx);
+    shape_nomod_idx = all_d{q}.shape_nomod_idx;
+    for k = 1:length(shape_nomod_idx); d_out.shape_nomod_idx( num_shape_nomod_idx + k ) = shape_nomod_idx(k) + num_tags; end;
     d_out.tags = [d_out.tags, all_d{q}.tags];
-    d_out.coverage = cat(2, d_out.coverage, all_d{q}.coverage);
-    d_out.total_coverage = d_out.total_coverage + all_d{q}.total_coverage;
+
+    if ~isfield(d_out,'norm_val') continue; end
     d_out.norm_val = [d_out.norm_val, all_d{q}.norm_val];    
     d_out.norm_idx = [];  % will fix in a minute!
+
+    if ~isfield(d_out,'mut_rate_matrix') continue; end
     d_out.mut_rate_matrix = cat(3, d_out.mut_rate_matrix, all_d{q}.mut_rate_matrix);
     d_out.rfcount_mut_rate_profiles = cat(2, d_out.rfcount_mut_rate_profiles, all_d{q}.rfcount_mut_rate_profiles);
     d_out.coverage_matrix = cat(2, d_out.coverage_matrix, all_d{q}.coverage_matrix);
 end
 
 
+if ~isfield(d_out,'norm_val') return; end
+
 % RENORM
 norm_idx = figure_out_idx_for_normalization( d_out.total_coverage );
-if length(norm_idx)>0;
+if ~isempty(norm_idx)
     for i = 1:size(d_out.r_norm,3)
         r(:,:,i)     = d_out.r_norm(:,:,i)*d_out.norm_val(i);
         r_err(:,:,i) = d_out.r_norm_err(:,:,i)*d_out.norm_val(i);
     end
     [d_out.r_norm, d_out.r_norm_err,~,d_out.norm_val] = normalize_reactivity(r,r_err,norm_idx,d_out.BLANK_OUT5, d_out.BLANK_OUT3, d_out.conditions, [], d_out.sequences );
 end
+
 d_out.norm_idx = norm_idx;
 
 
--- matlab/output/output_ubr_hdf5.m
+++ matlab/output/output_ubr_hdf5.m
@@ -1,20 +1,50 @@
-function output_ubr_hdf5( filename, d, VERBOSE );
+function output_ubr_hdf5( filename, d, VERBOSE, fields, section );
 % output_ubr_hdf5( filename, d );
 %
-%  Works generally on any MATLAB workspace, but
+%  Works generally on any MATLAB struct, but
 %   can have special treatment of shape_nomod_idx and
 %   other variables in  quick_look_ubr().
 %
 % Inputs:
 %  filename = name of output HDF5-formatted file.
-%  d        = MATLAB workspace 
-%  VERBOSE  = verbose output (Default 0)
+%  d        = MATLAB struct (or optionally, cell of struts)
 %
-% (C) R. Das, Stanford University & HHMI 2024
-if ~exist('VERBOSE','var'); VERBOSE = 0; end; 
-fields = fieldnames(d);
+% Optional:
+%  VERBOSE  = verbose output (Default 1)
+%  fields   = cell of strings: which fields to use (Default: all fields).
+%               Can be cell of cell of strings if multiple structs are
+%               input for d
+%  section = (string) names of section(s) (Default: [], no sections). 
+%                Can be cell of strings if multiple structs are input for d.
+%
+% (C) R. Das, Stanford University & HHMI 2024-2025
+if ~exist('VERBOSE','var'); VERBOSE = 1; end; 
+if ~exist('fields','var'); fields = []; end; % all fields
+if ~exist('section','var'); section = []; end; % no section
+
+all_d = d; all_fields = fields; all_sections = section;
+if isstruct(d); all_d = {all_d}; all_fields = {all_fields}; all_sections = {section}; end;
+
 if exist(filename,'file'); delete(filename); end;
 if VERBOSE; fprintf('Outputting to HDF5 file: %s\n',filename); end;
+
+for i = 1:length(all_d);
+    d = all_d{i};
+    fields = all_fields{i};
+    section = all_sections{i};
+    output_struct( d, fields, section, filename, VERBOSE )
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function output_struct( d, fields, section, filename, VERBOSE )
+if isempty(fields)
+    fields = fieldnames(d);
+end
+section_tag = '';
+if ~isempty(section); 
+    section_tag = ['/',section]; 
+    fprintf('\nOutputting section: %s\n',section)
+end
 for i = 1:length(fields)
     fieldName = fields{i};
     data = d.(fieldName);
@@ -24,13 +54,13 @@ for i = 1:length(fields)
         if iscell(data); data = string(data); end;
         if isnumeric(data) | isstring(data)
             sz = fliplr(size(data));
-            h5create(filename,['/',fieldName],sz,'Datatype',class(data));
-            h5write(filename,['/',fieldName],permute(data,length(sz):-1:1) );
+            h5create(filename,[section_tag,'/',fieldName],sz,'Datatype',class(data));
+            h5write(filename,[section_tag,'/',fieldName],permute(data,length(sz):-1:1) );
         elseif ischar(data)
             data = string(data);
             sz = size(data);
-            h5create(filename,['/',fieldName],sz,'Datatype','string');
-            h5write(filename,['/',fieldName], data );
+            h5create(filename,[section_tag,'/',fieldName],sz,'Datatype','string');
+            h5write(filename,[section_tag,'/',fieldName], data );
         else
             warning( ['Did not output non-empty field: ',fieldName] );
         end
